e0ae966f97690e7be05275e7ecb6f36190d1a20c
iMAC 2015 <38024526+NitramDivad@users.noreply.github.com>
2018-07-08 23:22:43 -0600

    Updated Files from LONG Sunday

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,207 +1,113 @@
-
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBey8_-49En0YiaiypnAgvKRhwhxroplKA",
-    authDomain: "trainscheduler-2e88e.firebaseapp.com",
-    databaseURL: "https://trainscheduler-2e88e.firebaseio.com",
-    projectId: "trainscheduler-2e88e",
-    storageBucket: "",
-    messagingSenderId: "88617351484"
-  };
-  firebase.initializeApp(config);
-
-
-
-
-
-
-// var StarTrek = {
-//     arrGifTopics: [],
-//     offSet: 0,
-//     currentTopic: "",
-
-//     BuildTheButtons: function() {
-
-//         $(".gifButtons").empty();
-
-//         this.arrGifTopics.forEach(function(item) {
-//             var button = $("<button>");
-//             button.addClass("btn btn-primary btn-topic");
-//             button.attr("data-name",item);
-//             button.text(item);
-//             $(".gifButtons").append(button);
-//         });
-//     },
-
-//     AddAButton: function(newTopic) {
-
-//         this.arrGifTopics.push(newTopic);
-//         $("#input_StarTrek").val("");
-
-//         this.BuildTheButtons();
-//     },
-
-//     CallAJAX: function(topic) {
-//         var apiURL = "https://api.giphy.com/v1/gifs/search?q=",
-//             apiKey = "&api_key=d03x0UXViZqHxHo74p52vH3b2iTSF52G",
-//             apiLimit = "&limit=10",
-//             apiOffset = "&offset=" + (this.offSet).toString(),
-//             queryURL,
-//             multiTopic;
-            
-            
-//         if (topic.length === 1)
-//             queryURL = apiURL + topic + apiKey + apiLimit;
-//         else {
-//             for (i=0; i < topic.length; i++) {
-//                 if (i > 0)
-//                     multiTopic += "+" + topic[i];
-//                 else
-//                     multiTopic = topic[i];
-//             }
-
-//             queryURL = apiURL + multiTopic + apiKey + apiLimit;
-//         }
-
-//         if (this.offSet > 0)
-//             queryURL += apiOffset;
-
-//         $.ajax({
-//             url: queryURL,
-//             method: "GET"
-//             }).then(function(response) {
-//                 console.log((response.data).length)
-//                 if ((response.data).length > 0)
-//                     StarTrek.ProcessAJAX(response.data)
-//                 else {
-//                     $("#add-ten").addClass("d-none");
-//                     $("#no-records").removeClass("d-none");
-//                 }
-//             });
-//     },
-
-//     ProcessAJAX: function(topicGifs) {
-//         var gifDiv,
-//             gifURL,
-//             stillURL,
-//             gifImage,
-//             gifRating,
-//             rating;
-
-//         $("#no-records").addClass("d-none");
-//         $("#add-ten").removeClass("d-none");
-
-//         topicGifs.forEach(function(item) {
-
-//             gifURL = item.images.fixed_height.url;
-//             stillURL = item.images.fixed_height_still.url;
-//             gifRating = (item.rating).toUpperCase();
-
-//             gifDiv = $("<div>");
-//             gifDiv.addClass("st_gifDiv");
-//             gifImage = $("<img class='gifImage'>" + "<br>");
-//             gifImage.attr("src", stillURL);
-//             gifImage.attr("alt", item.images.title);
-//             gifImage.attr("data-still", stillURL);
-//             gifImage.attr("data-animate", gifURL);
-//             gifImage.attr("data-state", "still");
-
-//             rating = $('<p class="gifRating">').text(gifRating + " Rating");
-
-//             gifDiv.prepend(gifImage);
-//             gifDiv.append(rating);
-                
-//             if (this.offSet === 0)
-//                 $(".gifsGrouping").append(gifDiv);
-//             else
-//                 $(".gifsGrouping").prepend(gifDiv);
-//         })
-//     },
-
-//     PlayGif: function(whichOne) {
-
-//         $(whichOne).attr("src", $(whichOne).attr("data-still"));
-//         $(whichOne).attr("data-state", "still");
-//     },
-
-//     PauseGif: function(whichOne) {
-
-//         $(whichOne).attr("src", $(whichOne).attr("data-animate"));
-//         $(whichOne).attr("data-state", "animate");
-//     }
-// };
-
-
-// //***************************************************************/
-// $(document).ready(function() {
-// //***************************************************************/
-
-//     StarTrek.arrGifTopics = ["Captain Kirk","Captain Picard",
-//                              "Klingon","Romulan",
-//                              "Ferengi","Wrath of Khan",
-//                              "Tribbles","Sulu",
-//                              "Spock","Uhura"
-//                             ];
-
-//     StarTrek.BuildTheButtons();
-// });
-
-
-// //***************************************************************/
-// $("#add-topic").bind("click", function(event) {
-// //***************************************************************/
-
-//     event.preventDefault(); 
-
-//     strInput = $("#input_StarTrek").val().trim();
-//     if (strInput != '') 
-//         StarTrek.AddAButton(strInput);
-// });
-
-
-// //***************************************************************/
-// $('#input_StarTrek').keypress(function(event) {
-// //***************************************************************/   
-
-//     if (event.keyCode == '13')      //ensures ENTER does not refresh
-//         event.preventDefault();
-//  });
-
-
-// //***************************************************************/
-// $(document).on("click", ".btn-topic", function() {
-// //***************************************************************/
-
-//     var whichTopic = $(this).attr("data-name"),
-//         arrWhichTopic = whichTopic.match(/\b(\w+)\b/g)  //splits out to array separate words
-
-//     $(".gifsGrouping").empty();
-//     StarTrek.numRequests = 0;
-//     $("#add-ten").addClass("d-none");
-
-//     StarTrek.currentTopic = arrWhichTopic;
-//     StarTrek.CallAJAX(arrWhichTopic);
-// });
-
-
-// //***************************************************************/
-// $(document).on("click" , ".gifImage" , function() {
-// //***************************************************************/
-
-//     var state = $(this).attr("data-state");
+var TrainSchedule = {
+        database: {},
+        trainName: '',
+        destination: '',
+        firstDeparture: '',
+        frequency: 0,
+        trainAdded: false,
     
-//     if (state === "still")
-//         StarTrek.PauseGif(this);
-//     else
-//         StarTrek.PlayGif(this);
-// });
+        InitializeFirebase: function() {
+        
+            var config = {
+                apiKey: "AIzaSyBey8_-49En0YiaiypnAgvKRhwhxroplKA",
+                authDomain: "trainscheduler-2e88e.firebaseapp.com",
+                databaseURL: "https://trainscheduler-2e88e.firebaseio.com",
+                projectId: "trainscheduler-2e88e",
+                storageBucket: "",
+                messagingSenderId: "88617351484"
+            };
+
+            firebase.initializeApp(config);
+            this.database = firebase.database();
+        },
+
+        AddTrain: function() {
+            var trainToAdd;
+
+            this.trainName = $("#train-name").val().trim(); 
+            this.destination = $("#destination").val().trim();
+            this.firstDeparture = $("#first-departure").val().trim();
+            this.frequency = $("#frequency").val().trim();
+
+            trainToAdd = {
+                TrainName: this.trainName,
+                Destination: this.destination,
+                FirstDeparture: this.firstDeparture,
+                Frequency: this.frequency
+            };
+
+            this.trainAdded = true;
+            this.database.ref().push(trainToAdd);
+        },
+
+        PopulateSchedule: function(snapshot) {
+
+            this.trainName = snapshot.val().TrainName;
+            this.destination = snapshot.val().Destination;
+            this.firstDeparture = snapshot.val().FirstDeparture;
+            this.frequency = snapshot.val().Frequency;
+
+            var newRow = $("<tr>").append(
+                $("<td>").text(this.trainName),
+                $("<td>").text(this.destination),
+                $("<td>").text(this.frequency),
+                $("<td>").text(TrainSchedule.NextTrain()),
+                $("<td>").text(TrainSchedule.MinutesAway())
+            );
+  
+            $(".table > tbody").append(newRow);
+
+            if (this.trainAdded) {
+                $("tbody tr:last").effect("highlight", {}, 6000);
+                this.trainAdded = false;
+            }
+        },
+
+        NextTrain: function() {
+            var arrivalTime = moment().add(TrainSchedule.MinutesAway(), "minutes");
+        
+            return moment(arrivalTime).format("hh:mm A");
+        },
+
+        MinutesAway: function() {
+            var formattedDepart = moment(this.firstDeparture, "HH:mm"),
+                timeDifference = moment().diff(moment(formattedDepart), "minutes"),
+                remainingMins = timeDifference % this.frequency;
+
+            if (timeDifference >= 0)
+                return this.frequency - remainingMins;
+            else
+                return moment(formattedDepart).diff(moment(), "minutes") + 1;
+        },
+
+        ClearInputs: function() {
+
+            $("#train-name").val("");
+            $("#destination").val("");
+            $("#first-departure").val("");
+            $("#frequency").val("");
+        }
+    }
 
 
-// //***************************************************************/
-// $(document).on("click" , "#add-ten" , function() {
-// //***************************************************************/
-    
-//     StarTrek.offSet += 10;
-//     StarTrek.CallAJAX(StarTrek.currentTopic);
-// });
+    /***************************************************************/
+    $(document).ready(function() {
+    /***************************************************************/
+
+        TrainSchedule.InitializeFirebase();
+
+        TrainSchedule.database.ref().on("child_added", function(childSnapshot) {
+
+            TrainSchedule.PopulateSchedule(childSnapshot);
+        });
+    });
+
+
+    /***************************************************************/
+    $("#btn_addTrain").click(function(event) {
+    /***************************************************************/
+
+        event.preventDefault();
+        TrainSchedule.AddTrain();
+        TrainSchedule.ClearInputs();
+    });
